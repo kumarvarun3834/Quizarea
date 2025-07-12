477224cb2b4d9c1d5a2f15d45176fe3b0f1dd94e
kumarvarun3834 <154826151+kumarvarun3834@users.noreply.github.com>
2025-07-12 16:52:33 +0530

    started end screen

---
+++ lib/result_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:quizarea/TextContainer.dart';
+
+class ResultScreen extends StatelessWidget {
+  final List<List<String>> quizResult;
+  // final int originalOptionsPerQuestion;
+
+  const ResultScreen(this.quizResult, {super.key});
+
+  int getScoreForQuestion(List<String> result) {
+    return 0;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+
+    return ;
+  }
+}
