af23f58cafcf10c9953b26c9d0d94b611ba57e07
kumarvarun3834 <154826151+kumarvarun3834@users.noreply.github.com>
2025-07-25 09:27:54 +0530

    done till result screen filtering and loadout now remain to enter data in it

--- lib/result_screen.dart
+++ lib/result_screen.dart
@@ -13,7 +13,41 @@ class ResultScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    List<Widget> result_data = [];
+    int i = 0;
+    while (i < quizResult.length) {
+      Map<String, Object> data = quizResult[i];
+      Container(
+          margin: const EdgeInsets.all(20),
+          width: double.infinity,
+          child: Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Container(
+                width: MediaQuery.of(context).size.width * 0.2, // 20% width
+                height: 100,
+                color: Colors.green,
+                  child: Center(child:TextContainer(i.toString(), Colors.white, 15))
+              ),
+                Container(
+                  width: MediaQuery.of(context).size.width * 0.7, // 70% width
+                  height: 100,
+                  color: Colors.orange,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    crossAxisAlignment: CrossAxisAlignment.stretch,
+                    children: [],),
+                ),],
+            )
+      );
+      i++;
+    }
 
-    return TextContainer("text", Colors.white, 30);
+    // return TextContainer("text", Colors.white, 30);
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: result_data,
+    );
   }
 }
